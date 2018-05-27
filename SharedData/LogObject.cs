b20a18fb58331f8cfff76a4c93bbc1ad648e66eb
oriel351 <oriel351@gmail.com>
2018-05-27 23:54:21 +0300

    adde logobject for sapphier

---
+++ SharedData/LogObject.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedData
+{
+    public class LogObject
+    {
+        private string data;
+        private MessageTypeEnum mtype;
+        public string EnumType
+        {
+            get { return Enum.GetName(typeof(MessageTypeEnum), mtype); }
+            set { this.mtype = (MessageTypeEnum)Enum.Parse(typeof(MessageTypeEnum), value); }
+        }
+
+        public string Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+    }
+}
